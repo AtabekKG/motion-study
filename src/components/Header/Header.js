c811842f43b8c6e33cdb6a7e12ea665ab96e468d
Atabek <00rustambekov@gmail.com>
2023-02-16 18:02:39 +0600

    Added responsive size of Home

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,50 +1,53 @@
-/* eslint-disable react/jsx-no-undef */
 import React from "react";
-import { NavLink } from "react-router-dom";
+import {NavLink} from "react-router-dom";
 import Logo from "./../../image/logo.svg";
-import { CiSearch } from "react-icons/ci";
+import {CiSearch} from "react-icons/ci";
+import BurgerMenu from "./Burger-menu";
 
 const Header = () => {
-  return (
-    <header id="header">
-      <div className="container">
-        <div className="header">
-          <div className="header_logo">
-            <NavLink to={"/Home"}>
-              <img src={Logo} />
-            </NavLink>
-          </div>
-          <div className="header_right">
-            <nav className="header_right-nav">
-              <NavLink className="header_right-nav_item" to={"/Home"}>
-                Home
-              </NavLink>
-              <NavLink className="header_right-nav_item" to={"/About"}>
-                About Us
-              </NavLink>
-              <NavLink className="header_right-nav_item" to={"/Study"}>
-                Study Abroad
-              </NavLink>
-              <NavLink className="header_right-nav_item">
-                Contacts
-              </NavLink>
-            </nav>
-            <form className="header_right-search">
-              <CiSearch className="header_right-search_icon" />
-              <input type="text" placeholder="Поиск..." />
-            </form>
-            <div className="header_right-langs">
-              <select>
-                <option>ENG</option>
-                <option>RU</option>
-                <option>KG</option>
-              </select>
-            </div>
-          </div>
-        </div>
-      </div>
-    </header>
-  );
+    return (
+        <>
+            <header id="header">
+                <div className="container">
+                    <div className="header">
+                        <div className="header_logo">
+                            <NavLink to={"/Home"}>
+                                <img src={Logo}/>
+                            </NavLink>
+                        </div>
+                        <div className="header_right">
+                            <nav className="header_right-nav">
+                                <NavLink className="header_right-nav_item" to={"/Home"}>
+                                    Home
+                                </NavLink>
+                                <NavLink className="header_right-nav_item" to={"/About"}>
+                                    About Us
+                                </NavLink>
+                                <NavLink className="header_right-nav_item" to={"/Study"}>
+                                    Study Abroad
+                                </NavLink>
+                                <NavLink className="header_right-nav_item">
+                                    Contacts
+                                </NavLink>
+                            </nav>
+                            <form className="header_right-search">
+                                <CiSearch className="header_right-search_icon"/>
+                                <input type="text" placeholder="Поиск..."/>
+                            </form>
+                            <div className="header_right-langs">
+                                <select>
+                                    <option>ENG</option>
+                                    <option>RU</option>
+                                    <option>KG</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <BurgerMenu/>
+        </>
+    );
 };
 
 export default Header;
